2c2d43363e1b89636981e3575e0aa53abdb61a7d
meenaljo2006 <meenaljoshi2006@gmail.com>
2025-08-01 23:54:34 +0530

    addPrintLine

--- Arrays/removeDuplicates.java
+++ Arrays/removeDuplicates.java
@@ -54,6 +54,7 @@ public class removeDuplicates {
         int[] arr = {1, 1, 2, 2, 3, 4, 5};
         int []result = removeDup(arr);
         int[] arr2 = {3,1,2,1,3,2};
+        System.out.println(Arrays.toString(result));
         System.out.println(Arrays.toString(removeDuplicates(arr2)));
     }
     
