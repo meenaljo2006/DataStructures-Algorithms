0ed9209f37ae82699d0ab6463a23d8122bdb08b8
meenaljo2006 <meenaljoshi2006@gmail.com>
2025-08-05 21:59:32 +0530

    Concept & Basic Questions

--- Array List/reverse.java
+++ Array List/reverse.java
@@ -5,8 +5,8 @@ public class reverse {
     
     public static void main(String[] args) {
 
-       //printing reverse of an ArrayList 
-        ArrayList <Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5)) ;
+       //printing reverse of an ArrayList -- TC = O(N) SC = O(1)
+        ArrayList <Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5)) ; 
         for(int i=list.size();i>0;i--){
             System.out.println(list.get(i-1));
         }
@@ -14,3 +14,5 @@ public class reverse {
     }
     
 }
+
+
