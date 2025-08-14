82ac8ddc40c7e3bdb74c9789c39792314c934f87
meenaljo2006 <meenaljoshi2006@gmail.com>
2025-08-14 22:39:58 +0530

    Two Pointer Approach

--- Array List/pairSum2.java
+++ Array List/pairSum2.java
@@ -1,29 +1,36 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class pairSum {
+public class pairSum2 {
 
     public static void main(String[] args) {
-        
-        ArrayList <Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
-        int target = 5;
-        int sum =0;
 
-        int leftPtr = 0;
-        int rightPtr = list.size()-1;
+        //ciruclarly two pointer approach beacuse it is not completely  sorted it is rotated
+        ArrayList <Integer> list = new ArrayList<>(Arrays.asList(5, 6, 7, 8, 9, 1, 2, 3, 4));
+        int target = 10;
+        int sum=0;
+
+        int leftPtr =0;
+        int rightPtr=list.size()-1;
+
         while(leftPtr<rightPtr){
+
             sum = list.get(rightPtr)+list.get(leftPtr);
             if(sum==target){
                 System.out.println(leftPtr);
                 System.out.println(rightPtr);
                 break;
-            } else if(sum<target){
-                leftPtr++;
             } else if(sum>target){
+                leftPtr++;
+            } else if (sum<target){
                 rightPtr--;
             }
-        }
 
+
+
+
+        }
+        
     }
     
 }
