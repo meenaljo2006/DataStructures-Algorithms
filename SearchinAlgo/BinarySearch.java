82850d0a4396250477f49125a1e37953b11b894e
meenaljo2006 <meenaljoshi2006@gmail.com>
2025-08-23 12:52:03 +0530

    binarySearchAlgo

---
+++ SearchinAlgo/BinarySearch.java
@@ -0,0 +1,40 @@
+package SearchinAlgo;
+
+public class BinarySearch {
+
+    public static int binary(int[]arr,int search){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<=end){
+            int mid = (start+(end-start))/2;
+            if(arr[mid]==search){
+                return mid;
+            } else if(arr[mid]>search){
+                end = mid-1;
+            } else{
+                start = mid+1;
+            }
+        }
+
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,45,50,55,60,67,78,79};
+        int search = 45;
+        int result = binary(arr, search);
+        if(result ==-1){
+            System.out.println("Element not found");
+        } else{
+            System.out.println("Element found at index "+result);
+        }
+
+        
+    }
+
+
+
+    
+}
